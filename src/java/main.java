9c68eeef43806d0727fafa9923b55bff8eefa930
Hasan-Vural <108330009+Hasan-Vural@users.noreply.github.com>
2024-12-22 01:46:40 +0300

    Added new pages and BorderPane to Login fxml

--- src/java/main.java
+++ src/java/main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class App {
+public class main {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to CineJava!");
     }
